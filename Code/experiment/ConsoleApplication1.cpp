163b7250755f81b68ce571e43bee117892e2bc3a
stupid-vegetable-bird <97822083+stupid-vegetable-bird@users.noreply.github.com>
2023-12-22 16:55:50 +0800

    Add files via upload

---
+++ Code/experiment/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <Windows.h>
+#include <GL/glut.h>
+void mydisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glEnd(); glFlush();
+}
+int main(int argc, char** argv) {
+	glutCreateWindow("simple");
+	glutDisplayFunc(mydisplay);
+	glutMainLoop();
+}
